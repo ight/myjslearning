c06b2a9f348b0e4335a4472acd6c86c6a7c9bc2f
ight <thelight.prakash247@gmail.com>
2019-02-11 23:29:31 +0530

    Ight: Assign symbol copy

---
+++ object-fundas/coypingAccessor.js
@@ -0,0 +1,34 @@
+// first time encounter getter method in js
+var obj = {
+  foo: 1,
+  get bar() {
+    return 2;
+  }
+};
+
+var copy = Object.assign({}, obj);
+console.log(copy); // { foo: 1, bar: 2 }, the value of copy.bar is obj.bar's getter's return value.
+
+
+// This is an assign function that copies full descriptors
+function completeAssign(target, ...sources) {
+  sources.forEach(source => {
+    let descriptors = Object.keys(source).reduce((descriptors, key) => {
+      descriptors[key] = Object.getOwnPropertyDescriptor(source, key);
+      return descriptors;
+    }, {});
+    //by default, Object.assign copies enumerable Symbols too
+    Object.getOwnPropertySymbols(source).forEach(sym => {
+      let descriptors = Object.getOwnPropertyDescriptor(source, sym);
+      if (descriptor.enumerable){
+        descriptors[sym] = descriptor;
+      }
+    });
+    Object.defineProperties(target, descriptors);
+  });
+  return target;
+}
+
+var copy = completeAssign({}, obj);
+console.log(copy);
+// { foo: 1, get bar() { return 2} }
