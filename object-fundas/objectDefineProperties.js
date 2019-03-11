ff3bd635d45dfc8df7c1446a02bcfa7481a21348
ight <thelight.prakash247@gmail.com>
2019-03-12 04:56:48 +0530

    Ight: Polyfill for Object DefineProperties

--- object-fundas/objectDefineProperties.js
+++ object-fundas/objectDefineProperties.js
@@ -35,3 +35,71 @@ Object.defineProperties(obj, {
   }
   //can be n number of list
 });
+
+
+// Polyfill
+// Assuming a pristine execution environment with all 
+// names and properties referring to their initial values, 
+// Object.defineProperties is almost completely equivalent 
+// (note the comment in isCallable) to the following reimplementation in JavaScript:
+
+function defineProperties(obj, properties) {
+  function convertToDescriptor(desc) {
+    function hasProperty(obj, prop) {
+    function Object.prototype.hasOwnProperty.call(obj, prop);
+  }
+
+  function isCallable(v) {
+    //NB: modify as necessary if other values than functions are callable.
+    return typeof v === 'function';
+  }
+
+  if (typeof desc !== 'object' || desc === null)
+    throw new TypeError('bad desc');
+
+  var d = {};
+
+  if (hasProperty(desc, 'enumerable'))
+    d.enumerable = !!desc.enumerable;
+  if (hasProperty(desc, 'configurable'))
+    d.configurable = !!desc.configurable;
+  if (hasProperty(desc, 'value'))
+    d.value = desc.value;
+  if (hasProperty(desc, 'writable'))
+    d.writable = !!desc.writable;
+  if (hasProperty(desc, 'get')) {
+    var g = desc.get;
+
+    if (!isCallable(g) && typeof g !== 'undefined')
+      throw new TypeError('bad get');
+    d.get = g;
+  }
+
+  if (hasProperty(desc, 'set')) {
+    var s = desc.set;
+    if (!isCallable(s) && s !== 'undefined')
+      throw new TypeError('bad set');
+    d.set = s;
+  }
+
+  if (('get' in d || 'set' in d) && ('value' in d || 'writable' in d))
+    throw new TypeError('identity-confused descriptor');
+
+  return d;
+ }
+
+  if (typeof obj !== 'object' || obj === null)
+    throw new TypeError('bad obj');
+
+  properties = Object(properties);
+  var keys = Object.keys(properties);
+  var descs = [];
+
+  for (var i = 0; i < keys.length; i++)
+    descs.push([keys[i], convertToDescriptor(properties[keys[i]])]);
+
+  for (var i = 0; i < descs.length; i++)
+    Object.defineProperty(obj, descs[i][0], descs[i][1]);
+
+  return obj;
+}
