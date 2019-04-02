f9a4b9d4a081fcaf9ecde7cef0772130fe5b5322
ight <thelight.prakash247@gmail.com>
2019-04-03 03:57:13 +0530

    Ight: Completed object entries

--- object-fundas/objectEntries.js
+++ object-fundas/objectEntries.js
@@ -36,4 +36,40 @@ console.log(Object.entries(myObj)); //[['foo', 'bar']]
 // non object argument will be coerced to an object
 console.log(Object.entries('foo'));
 
-// return an empty array for any primtive type
+// return an empty array for any primitive type, since primitives have no own properties
+console.log(Object.entries(100)); // []
+
+//iterate through key-values gracefully
+const obj = { a: 5, b: 7, c: 9 };
+for (const [key, value] of Object.entries(obj)) {
+  console.log(`${key} ${value}`); // "a 5", "b 7", "c 9"
+}
+
+//or using aray extras
+Object.entries(obj).forEach(([key, value]) => {
+  console.log(`${key} ${value}`); // // "a 5", "b 7", "c 9"
+});
+
+// The new Map() constructor accepts an iterable of entries. 
+// With Object.entries, you can easily convert from Object to Map:
+
+const obj = {foo: 'bar', baz: 42};
+const map = new Map(Object.entries(obj));
+console.log(map); // Map {foo: "bar", baz: 42}
+
+// Using Array Destructuring, you can iterate through objects easily.
+const obj = { foo: "bar", baz: 42};
+Object.entries(obj).forEach(([key, value]) => console.log(`${key} ${value}`)); // "foo: bar", "baz: 42"
+
+//Pollyfill
+if(!Object.entries) {
+  Object.entries = function(obj){
+    var ownPros = Object.keys( obj ),
+      i = ownPros.length,
+      resArray = new Array(i);//prellocated the array
+    while (i--)
+      resArray[i] = [ownPros[i], obj[ownPros[i]]];
+    return resArray;
+  };
+}
+
